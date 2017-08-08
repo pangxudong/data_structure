7e9fcc4d144cea5025450e97f1fe67fd263295d3
pangxudong <pangxudong0312@gmail.com>
2017-08-08 21:41:11 +0800

    no message

---
+++ src/Alibaba.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Created by yuanxu on 17/4/21.
+ */
+public class Alibaba {
+
+    public static String search(String str) {
+        return "ab";
+    }
+
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        System.out.println(search(str));
+    }
+}
